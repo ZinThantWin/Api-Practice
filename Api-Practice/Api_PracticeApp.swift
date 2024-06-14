6069e6aecf9b23b40fee40b1ebfea58390607218
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-14 13:38:04 +0630

    Initial Commit

---
+++ Api-Practice/Api_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_PracticeApp.swift
+//  Api-Practice
+//
+//  Created by ကင်ဇို on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Api_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
